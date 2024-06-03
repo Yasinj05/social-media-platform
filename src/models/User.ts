563cf00f642944125497bb68390468f97f85fcfd
Yasin Jafari <yasinj7658@gmail.com>
2024-06-03 21:23:13 +0330

    Add User model

---
+++ src/models/User.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcryptjs";
+import { Schema, model, Document } from "mongoose";
+
+interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+const UserSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+// Hash password before saving
+UserSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = model<IUser>("User", UserSchema);
+export default User;
