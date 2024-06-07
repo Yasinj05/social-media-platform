c31ddac7bc27da1ba53a8d13b1903800a6affb81
Yasin Jafari <yasinj7658@gmail.com>
2024-06-07 20:45:32 +0330

    Update Posts route

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -25,4 +25,16 @@ router.get("/posts/:id", getPostById);
 // Delete a post
 router.delete("/posts/:id", auth, deletePost);
 
+// Like a post
+router.put("/posts/like/:id", auth, likePost);
+
+// Unlike a post
+router.put("/posts/unlike/:id", auth, unlikePost);
+
+// Comment on a post
+router.post("/posts/comment/:id", auth, commentOnPost);
+
+// Delete a comment
+router.delete("/posts/comment/:id/:comment_id", auth, deleteComment);
+
 export default router;
