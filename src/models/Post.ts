1d71cbdc4812e87659b6b71fc37543e17313fef1
Yasin Jafari <yasinj7658@gmail.com>
2024-06-08 18:46:17 +0330

    Update Post model

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,35 +1,57 @@
-import { Schema, model, Document } from "mongoose";
+import { Schema, model, Document, Types } from "mongoose";
 
-interface IComment extends Document {
-  user: Schema.Types.ObjectId;
+export interface IComment extends Document {
+  user: Types.ObjectId;
   text: string;
   date: Date;
 }
 
 interface IPost extends Document {
-  user: Schema.Types.ObjectId;
+  user: Types.ObjectId;
   text: string;
-  likes: { user: Schema.Types.ObjectId }[];
+  likes: { user: Types.ObjectId }[];
   comments: IComment[];
   date: Date;
 }
 
 const PostSchema = new Schema<IPost>({
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  text: { type: String, required: true },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
   likes: [
     {
-      user: { type: Schema.Types.ObjectId, ref: "User" },
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
     },
   ],
   comments: [
     {
-      user: { type: Schema.Types.ObjectId, ref: "User" },
-      text: { type: String, required: true },
-      date: { type: Date, default: Date.now },
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
     },
   ],
-  date: { type: Date, default: Date.now },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Post = model<IPost>("Post", PostSchema);
