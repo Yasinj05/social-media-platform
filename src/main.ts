4779e901fe82baed92a8afa1ce42630b9af344e6
Yasin Jafari <yasinj7658@gmail.com>
2024-06-09 17:45:59 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth";
+import postRoute from "./routes/posts";
 dotenv.config();
 
 const app = express();
@@ -9,13 +10,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api", authRoute);
+app.use("/api", postRoute);
 
 mongoose
   .connect(process.env.MONGODB_URI!)
   .then(() => {
     console.log("Connected to the MongoDB...");
     app.listen(PORT, () => {
-      console.log(`server is Running on port ${PORT}`);
+      console.log(`Server is Running on port ${PORT}`);
     });
   })
   .catch((error) => {
