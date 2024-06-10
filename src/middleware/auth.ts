f748d83d0fb01fb732f2ea70d834d41c3465c08c
Yasin Jafari <yasinj7658@gmail.com>
2024-06-10 18:21:40 +0330

    Update auth.ts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -10,10 +10,16 @@ interface AuthRequest extends Request {
 }
 
 export default function (req: AuthRequest, res: Response, next: NextFunction) {
-  const token = req.header("x-auth-token");
-  if (!token) {
+  const authHeader = req.header("Authorization");
+  if (!authHeader) {
     return res.status(401).json({ message: "No token, authorization denied" });
   }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Token is missing" });
+  }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY!) as {
       user: { id: string };
@@ -21,6 +27,7 @@ export default function (req: AuthRequest, res: Response, next: NextFunction) {
     req.user = decoded.user;
     next();
   } catch (err) {
+    console.error("Token verification failed", err);
     res.status(401).json({ message: "Token is not valid" });
   }
 }
